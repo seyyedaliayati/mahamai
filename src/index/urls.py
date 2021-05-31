db5e9fc1f57af4bec8abd39bd451fb9309c65a73
Seyyed Ali Ayati <seyyedaliayati@gmail.com>
2021-05-31 22:38:28 +0430

    Added template static files

--- src/index/urls.py
+++ src/index/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from index.views import IndexView
+
 urlpatterns = [
-    
+    path('', IndexView.as_view(), name='index'),
 ]
--- src/mahamai/settings.py
+++ src/mahamai/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'mahamai.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +122,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
