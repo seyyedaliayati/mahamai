d5b8cbfbabc4440fc18b7ed2b97ca230ddbc86e5
moein <moeintebbi8@gmail.com>
2021-06-13 11:31:03 +0430

    creat model and modelform for work with us page

--- src/index/models.py
+++ src/index/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.forms.fields import EmailField
 
 
 class ContactUs(models.Model):
@@ -13,14 +14,14 @@ class ContactUs(models.Model):
         verbose_name_plural = "Contact Us Data"
 
 
-class WorkWithUs(models.Model):
-    first_name = models.CharField(max_length=128)
-    last_name = models.CharField(max_length=128)
+class workwithus(models.Model):
+    subject = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=130)
+    last_name = models.CharField(max_length=130)
     email = models.EmailField()
-    phone = models.CharField(max_length=12)
-    service = models.CharField(max_length=32)  # choice field
-    subject = models.CharField(max_length=128)
+    phone = models.CharField(max_length=30)
     message = models.TextField()
+    upload_file = models.FileField
 
     def __str__(self) -> str:
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name} {self.phone}"
